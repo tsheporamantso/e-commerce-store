34d23bb205ec06b32dd2836f43d68bc94cccfcfe
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-10 21:12:10 +0200

    feat(logo): add logo icon and link it back to home page

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { VscCode } from "react-icons/vsc";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Button size={"icon-lg"} asChild className="bg-blue-500 hover:bg-blue-600">
+      <Link href={"/"}>
+        <VscCode className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 };
 
 export default Logo;
