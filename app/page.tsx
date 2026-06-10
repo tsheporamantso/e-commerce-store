472fe3b307be4aa162cccf51b7da4c7bccab3dba
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-10 19:31:15 +0200

    chore(components): install shadcn ui components and suppress eslint rule

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl">Home Page</h1>
-      <Button variant={"outline"} size={"lg"} className="m-8 capitalize">
-        click me
+      <Button
+        variant={"destructive"}
+        size={"lg"}
+        className="m-8 capitalize"
+        asChild
+      >
+        <Link href={"/about"}>about</Link>
       </Button>
     </div>
   );
