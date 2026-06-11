d1db237656de72cf1876d6f6952b2cade7d48f63
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-11 05:46:31 +0200

    feat(nav search): add shadcn ui input component

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Input } from "../ui/input";
 
 const NavSearch = () => {
-  return <div>NavSearch</div>;
+  return (
+    <Input
+      type="search"
+      placeholder="search product..."
+      className="max-w-xs dark:bd-muted"
+    />
+  );
 };
 
 export default NavSearch;
