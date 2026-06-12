85f5800ed84b036d28de4969ac644ec36b9b7413
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-12 13:31:05 +0200

    feat(featured products): setup featured products component

--- components/home/FeaturedProducts.tsx
+++ components/home/FeaturedProducts.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { fetchFeaturedProducts } from "@/utils/actions";
+import EmptyList from "../global/EmptyList";
+import SectionTitle from "../global/SectionTitle";
+import ProductsGrid from "../product/ProductsGrid";
 
-const FeaturedProducts = () => {
-  return <div>FeaturedProducts</div>;
+const FeaturedProducts = async () => {
+  const products = await fetchFeaturedProducts();
+
+  if (products.length === 0) return <EmptyList />;
+  return (
+    <section className="pt-24">
+      <SectionTitle text="featured product" />
+      <ProductsGrid products={products} />
+    </section>
+  );
 };
 
 export default FeaturedProducts;
