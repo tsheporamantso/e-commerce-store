1c35c7b15006746edbf7b4f60e312a265ee2c010
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-12 13:49:52 +0200

    feat(toggle button): import and render shadcn ui button and fav heart react icon

--- components/product/FavoriteToggleButton.tsx
+++ components/product/FavoriteToggleButton.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { FaHeart } from "react-icons/fa";
+import { Button } from "../ui/button";
 
-const FavoriteToggleButton = () => {
-  return <div>FavoriteToggleButton</div>;
+const FavoriteToggleButton = ({ productId }: { productId: string }) => {
+  return (
+    <Button size={"icon"} variant={"outline"} className="p-2 cursor-pointer">
+      <FaHeart />
+    </Button>
+  );
 };
 
 export default FavoriteToggleButton;
