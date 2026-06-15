d54a79ae162e8bfc0a6ab398057d39ba45eb204b
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-15 10:07:39 +0200

    feat(home page): add react suspense component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import LoadingContainer from "@/components/global/LoadingContainer";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
 import Hero from "@/components/home/Hero";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <FeaturedProducts />
+      <Suspense fallback={<LoadingContainer />}>
+        <FeaturedProducts />
+      </Suspense>
     </>
   );
 }
