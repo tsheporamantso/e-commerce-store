6fe5cf2dd7e921257bc59c0e45ef7cc5a20b9d53
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-17 07:43:41 +0200

    refactor: update fetch all products function with complex search query

--- utils/actions.ts
+++ utils/actions.ts
@@ -9,8 +9,14 @@ export const fetchFeaturedProducts = async () => {
   return products;
 };
 
-export const fetchAllProducts = () => {
+export const fetchAllProducts = ({ search = "" }: { search: string }) => {
   return prisma.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: search, mode: "insensitive" } },
+        { company: { contains: search, mode: "insensitive" } },
+      ],
+    },
     orderBy: {
       createdAt: "desc",
     },
