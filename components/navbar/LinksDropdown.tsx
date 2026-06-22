ebb322a681234202491a257f929dd83ad902fb44
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-22 14:47:20 +0200

    feat: restric dashboard to only admin user

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -13,8 +13,11 @@ import { links } from "@/utils/links";
 import UserIcon from "./UserIcon";
 import { SignInButton, SignUpButton, Show } from "@clerk/nextjs";
 import SignOutLink from "./SignOutLink";
+import { auth } from "@clerk/nextjs/server";
 
-const LinksDropdown = () => {
+const LinksDropdown = async () => {
+  const { userId } = await auth();
+  const isAdmin = userId === process.env.ADMIN_USER_ID;
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -39,6 +42,7 @@ const LinksDropdown = () => {
         </Show>
         <Show when={"signed-in"}>
           {links.map((link) => {
+            if (link.label === "dashboard" && !isAdmin) return null;
             return (
               <DropdownMenuItem key={link.href}>
                 <Link href={link.href} className="capitalize w-full">
