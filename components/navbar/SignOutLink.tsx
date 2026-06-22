f9edadf2c4a19fa1cf02010790d8e30d9a315f12
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-22 09:26:10 +0200

    feat(signout): add signout link jsx

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+"use client";
 import { toast } from "sonner";
 import { SignOutButton } from "@clerk/nextjs";
 import Link from "next/link";
 
-const handleLogout = () => {
-  toast("Logging Out", { description: "Logged out successfully" });
-};
+// const handleLogout = () => {
+//   toast("Logging Out", { description: "Logged out successfully" });
+// };
 const SignOutLink = () => {
   return (
     <SignOutButton>
-      <Link href={"/"} className="w-full text-left" onClick={handleLogout}>
+      <Link
+        href={"/"}
+        className="w-full text-left"
+        onClick={() => toast.success("Logged out successfully.")}
+      >
         Logout
       </Link>
     </SignOutButton>
