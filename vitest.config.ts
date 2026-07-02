d156066a7cea9a712f6750e33ccf7cc4edc54d6d
tsheporamantso <tshepo.ramantso@outlook.com>
2026-07-02 10:42:30 +0200

    docs(config): add vitest config file

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+    globals: true, // lets you use describe/it/expect without importing them
+  },
+});
