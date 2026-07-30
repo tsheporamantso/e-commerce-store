8b29a829ac750cbca259c018b00fd662d4241dfc
tsheporamantso <tshepo.ramantso@outlook.com>
2026-07-30 14:45:49 +0200

    refactor: add default boolean value

--- components/form/CheckBoxInput.tsx
+++ components/form/CheckBoxInput.tsx
@@ -2,22 +2,28 @@
 
 import React from "react";
 import { Checkbox } from "../ui/checkbox";
+import { Label } from "../ui/label";
+
 type CheckBoxInputProps = {
   name: string;
   label: string;
   defaultChecked?: boolean;
 };
 
-const CheckBoxInput = ({ name, label, defaultChecked }: CheckBoxInputProps) => {
+const CheckBoxInput = ({
+  name,
+  label,
+  defaultChecked = false,
+}: CheckBoxInputProps) => {
   return (
     <div className="flex items-center space-x-2">
       <Checkbox id={name} name={name} defaultChecked={defaultChecked} />
-      <label
+      <Label
         htmlFor={name}
         className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 capitalize"
       >
         {label}
-      </label>
+      </Label>
     </div>
   );
 };
