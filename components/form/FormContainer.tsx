cba427ed40a504b6fc9473d2f5b062fd0ea9c66a
tsheporamantso <tshepo.ramantso@outlook.com>
2026-07-30 16:47:00 +0200

    feat: add form container component

---
+++ components/form/FormContainer.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import { useEffect } from "react";
+// import { useToast } from "@/components/ui/use-toast";
+import { toast } from "sonner";
+import { actionFunction } from "@/utils/types";
+
+const initialState = {
+  message: "",
+};
+
+const FormContainer = ({
+  action,
+  children,
+}: {
+  action: actionFunction;
+  children: React.ReactNode;
+}) => {
+  const [state, formAction] = useFormState(action, initialState);
+
+  useEffect(() => {
+    if (state.message) {
+      toast.success(state.message);
+    }
+  }, [state]);
+  return <form action={formAction}>{children}</form>;
+};
+
+export default FormContainer;
