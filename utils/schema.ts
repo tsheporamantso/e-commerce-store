bf668ea7c2c7933189fee4894950a3d4318bd135
tsheporamantso <tshepo.ramantso@outlook.com>
2026-07-31 19:06:06 +0200

    refactor: update product schema to throw meaningful messages

--- utils/schema.ts
+++ utils/schema.ts
@@ -1,9 +1,21 @@
 import { z } from "zod";
 
 export const productSchema = z.object({
-  name: z.string().min(4),
-  company: z.string().min(4),
-  price: z.coerce.number().int().min(0),
-  description: z.string,
+  name: z
+    .string()
+    .min(2, {
+      message: "name must be at least more than 2 characters.",
+    })
+    .max(100, {
+      message: "name must not exceed 100 characters.",
+    }),
+  company: z.string(),
   featured: z.coerce.boolean(),
+  price: z.coerce.number().int().min(0, {
+    message: "price must be a positive number.",
+  }),
+  description: z.string().refine((description) => {
+    const wordCount = description.split(" ").length;
+    return wordCount >= 10 && wordCount <= 100;
+  }),
 });
