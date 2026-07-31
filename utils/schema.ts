a461066a36bc73f16aa9326d42c6a160cc8b2570
tsheporamantso <tshepo.ramantso@outlook.com>
2026-07-31 18:23:45 +0200

    chore: add product schema validation with zod library

---
+++ utils/schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const productSchema = z.object({
+  name: z.string().min(4),
+  company: z.string().min(4),
+  price: z.coerce.number().int().min(0),
+  description: z.string,
+  featured: z.coerce.boolean(),
+});
