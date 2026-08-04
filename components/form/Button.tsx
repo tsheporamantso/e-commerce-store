53c3ea616dea180c6cb15f479c3841e6c4fb3de4
tsheporamantso <tshepo.ramantso@outlook.com>
2026-08-04 18:25:26 +0200

    feat: add edit and delete button component

--- components/form/Button.tsx
+++ components/form/Button.tsx
@@ -41,3 +41,32 @@ export function SubmitButton({
     </Button>
   );
 }
+
+type actionType = "edit" | "delete";
+
+export const IconButton = ({ actionType }: { actionType: actionType }) => {
+  const { pending } = useFormStatus();
+
+  const renderIcon = () => {
+    switch (actionType) {
+      case "edit":
+        return <LuPen />;
+      case "delete":
+        return <LuTrash2 />;
+      default:
+        const never: never = actionType;
+        throw new Error(`Invalid actiontype: ${never}`);
+    }
+  };
+
+  return (
+    <Button
+      type="submit"
+      size={"icon"}
+      variant={"link"}
+      className="p-2 cursor-pointer"
+    >
+      {pending ? <Spinner className="animate-spin" /> : renderIcon()}
+    </Button>
+  );
+};
